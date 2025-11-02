1cbe4ed1c7f4efe2084834b897d57d1dcd6bf0b7
sejalkumari12-ui <sejalkumari5679@gmail.com>
2025-11-02 08:33:00 +0530

    express middleware

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ import express from 'express'
 
 const app = express()
 
+app.use(function(req,res,next){
+  console.log("middleware working")
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
